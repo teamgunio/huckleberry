a8c841f1beebb9886370ac00334113e929eb9395
Danny Graham <good.midget@gmail.com>
2019-08-28 16:11:51 -0700

    fix: HUCK-2 fixes e2e tests to support API DB connections

--- stack/api/test/app.e2e-spec.ts
+++ stack/api/test/app.e2e-spec.ts
@@ -5,28 +5,35 @@ import { AppModule } from './../src/app.module';
 const { npm_package_version } = process.env;
 
 describe('AppController (e2e)', () => {
-  let app;
+  let app, server;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    server = app.getHttpServer()
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', (done) => {
+    request(server)
       .get('/')
       .expect(200)
       .expect(`Huckleberry v${npm_package_version} says, "Oh, hello."`);
+    server.close(done)
   });
 
-  it('/api/version (GET)', () => {
-    return request(app.getHttpServer())
+  it('/api/version (GET)', (done) => {
+    request(server)
       .get('/api/version')
       .expect(200)
       .expect(`${npm_package_version}`);
+    server.close(done)
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
