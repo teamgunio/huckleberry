3f1e7bcea564a469482f1010b4149709e2e8a9a1
Danny Graham <good.midget@gmail.com>
2019-08-31 14:40:58 -0700

    feat: HUCK-24 #done intercepts actions from doc and checks for a matching skill to run

--- stack/client/src/services/workflow.js
+++ stack/client/src/services/workflow.js
@@ -1,5 +1,6 @@
+import history from '../history';
 import { startOAuthFlow } from '../contexts/oauth';
-import { post } from './api';
+import { post, get } from './api';
 
 const flows = {
   addIntegration: async (params) => {
@@ -18,10 +19,20 @@ const flows = {
     setTimeout(() => {
       startOAuthFlow(type, integration);
     }, 3000);
-  }
+  },
 };
 
 export const handleMessage = async message => {
   const { action, parameters } = message;
   if (flows[action]) return flows[action](parameters.fields);
+  else {
+    let res = await get('skills');
+    const skills = await res.json();
+    const [skill] = skills.filter(s => s.action === action);
+
+    if (skill) {
+      await post(`skills/${skill.id}/run`);
+      history.push('/activities');
+    }
+  }
 };
